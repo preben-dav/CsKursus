93a23a712b00c32d8487069e0d2aa46278a71947
preben-dav <preben.dav@gmail.com>
2019-08-12 15:49:07 +0200

    Opg 40 opdateret

--- BrugAfVariabler/Program.cs
+++ BrugAfVariabler/Program.cs
@@ -6,7 +6,18 @@ namespace BrugAfVariabler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 10;
+            a++;
+            a--;
+            a += 20;
+            Console.WriteLine(a);
+
+            double b = 12.5;
+            Console.WriteLine(b);
+            b++;
+            b--;
+            b *= 2;
+            Console.WriteLine(b);
         }
     }
 }

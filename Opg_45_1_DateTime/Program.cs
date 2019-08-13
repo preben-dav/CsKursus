090efc21306fbd3b2abe1f34353402cb846f330d
preben-dav <preben.dav@gmail.com>
2019-08-13 11:12:07 +0200

    rename

---
+++ Opg_45_1_DateTime/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Opg_45_1_DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime a,b;
+            a = DateTime.Now;
+            a = new DateTime(2019, 8, 12);
+            Console.WriteLine($"a = {a}");
+            b=a.AddDays(1);
+            Console.WriteLine($"b = {b}");
+
+            Console.WriteLine($"Dag på året {a.DayOfYear}");
+
+            Console.WriteLine($"Ugedag {a.DayOfWeek.ToString()}");
+            Console.WriteLine($"Lille dato streng {a.ToShortDateString()}");
+            Console.WriteLine($"Stor dato streng {a.ToLongDateString()}");
+            Console.WriteLine($"Lille tid streng {a.ToShortTimeString()}");
+            Console.WriteLine($"Stor tid streng {a.ToLongTimeString()}");
+
+        }
+    }
+}

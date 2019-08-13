470cb5ff1ad07006c1bad22af09516c2518e32d3
preben-dav <preben.dav@gmail.com>
2019-08-13 13:10:20 +0200

    enum opgave tilføjet

---
+++ Opg_48_1_Enum/Classes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opg_48_1_Enum
+{
+    enum kortKulør
+    {
+        Spar=10,
+        Hjerter=20,
+        Ruder=30,
+        Klør=40
+    }
+}
---
+++ Opg_48_1_Enum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Opg_48_1_Enum
+{
+
+     class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int kort1Værdi = 2;
+            kortKulør kort1Kulør = kortKulør.Hjerter;
+
+            int kort2Værdi = 2;
+            kortKulør kort2Kulør = kortKulør.Klør;
+
+            Console.WriteLine(kort1Kulør);
+            Console.WriteLine(kort1Kulør.ToString());
+            Console.WriteLine((int)kort1Kulør);
+            Console.WriteLine(System.Convert.ToInt32(kort1Kulør));
+            Console.WriteLine();
+            Console.WriteLine(kort1Værdi);
+
+            Console.WriteLine();
+            Console.WriteLine(kort2Kulør);
+            Console.WriteLine(kort2Værdi);
+        }
+    }
+}
